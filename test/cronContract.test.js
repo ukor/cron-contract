8b3bef161b80f29b35e26760873c45b3fc601c9b
Jidechi ukor <ukorjidechi@gmail.com>
2021-05-17 01:57:04 +0100

    update package.json scripts

--- test/cronContract.test.js
+++ test/cronContract.test.js
@@ -7,7 +7,6 @@ describe('test cron contract', function () {
     beforeEach(async () => {
         [owner, addr1, addr2, _] = await ethers.getSigners();
         Token = await ethers.getContractFactory('CronContract');
-        console.log(owner.address);
         token = await Token.deploy(owner.address);
         await token.deployed();
     });
