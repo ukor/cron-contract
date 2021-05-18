fd9591be3e6439e78422d5a920e909109d83a771
Jidechi ukor <ukorjidechi@gmail.com>
2021-05-18 17:15:16 +0100

    Add ERC20 token contract with test case

--- contracts/valutContract.sol
+++ contracts/valutContract.sol
@@ -1,6 +1,168 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 
+contract CronToken {
+    mapping(address => uint256) private _balances;
+
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+
+    string private _name;
+    string private _symbol;
+
+    address public admin;
+
+    event Transfer(address from, address to, uint256 value);
+
+    event Approval(address from, address recipient, uint256 amount);
+
+    constructor(address _admin) {
+        _name = 'Cron';
+        _symbol = 'CRON';
+        admin = _admin;
+        mint(100000000 * 10**decimals());
+    }
+
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public pure returns (uint8) {
+        return 16;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    /// Transfer value from sender to recipient
+    function transfer(address recipient, uint256 amount) public returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+
+        return true;
+    }
+
+    /// Returns the amount of value a spender is allowed to spend on behalf of the owner
+    function allowance(address _owner, address spender) public view returns (uint256) {
+        return _allowances[_owner][spender];
+    }
+
+    // Set amount that a spender is allowed to spend on behalf of the sender
+    function approve(address spender, uint256 amount) public returns (bool) {
+        _approve(msg.sender, spender, amount);
+
+        return true;
+    }
+
+    /// Allows spender transfer from owner using allowance
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) public returns (bool) {
+        _transfer(sender, recipient, amount);
+
+        uint256 currentAllowance = _allowances[sender][msg.sender];
+        require(currentAllowance >= amount, 'Amount exceeds allowance');
+        _approve(sender, msg.sender, currentAllowance - amount);
+
+        return true;
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
+        uint256 currentAllowance = _allowances[msg.sender][spender];
+        _approve(msg.sender, spender, currentAllowance + addedValue);
+        return true;
+    }
+
+    // Create new token
+    function mint(uint256 _amount) public onlyAdmin returns (bool) {
+        _mint(msg.sender, _amount);
+
+        return true;
+    }
+
+    // destroy tokens
+    function burn(uint256 _amount) public onlyAdmin returns (bool) {
+        _burn(msg.sender, _amount);
+
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
+        uint256 currentAllowance = _allowances[msg.sender][spender];
+        require(currentAllowance >= subtractedValue, 'Decreased allowance below zero');
+        _approve(msg.sender, spender, currentAllowance - subtractedValue);
+
+        return true;
+    }
+
+    /// Set amount a spender can spend on behalf of the owner
+    function _approve(
+        address _owner,
+        address spender,
+        uint256 amount
+    ) internal {
+        require(_owner != address(0), 'Approve from zero address');
+        require(spender != address(0), 'Approve to zero address');
+
+        _allowances[_owner][spender] = amount;
+        emit Approval(_owner, spender, amount);
+    }
+
+    function _transfer(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) internal virtual {
+        require(sender != address(0), 'Transfering from zero address');
+        require(recipient != address(0), 'Transfering to zero address');
+
+        uint256 senderBalance = _balances[sender];
+        require(senderBalance >= amount, 'Insuffiencet balance');
+        _balances[sender] = senderBalance - amount;
+        _balances[recipient] += amount;
+
+        emit Transfer(sender, recipient, amount);
+    }
+
+    /// create new tokens and emit event from the zero address
+    function _mint(address account, uint256 amount) internal {
+        require(account != address(0), 'Mint to the zero address');
+
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+    }
+
+    /// Destroy tokens - Send amount
+    function _burn(address account, uint256 amount) internal {
+        require(account != address(0), 'Burn from the zero address');
+
+        uint256 accountBalance = _balances[account];
+        require(accountBalance >= amount, 'Burn amount exceeds balance');
+        _balances[account] = accountBalance - amount;
+        _totalSupply -= amount;
+
+        emit Transfer(account, address(0), amount);
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, 'Only owner can perform this operation');
+
+        _;
+    }
+}
+
 /*
     ## Overview
 
---
+++ test/token.test.js
@@ -0,0 +1,53 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+describe('Test token contract', function () {
+    let Token, token, admin, addr1, addr2;
+
+    beforeEach(async () => {
+        [admin, addr1, addr2, _] = await ethers.getSigners();
+        Token = await ethers.getContractFactory('CronToken');
+        token = await Token.deploy(admin.address);
+        await token.deployed();
+    });
+
+    describe('Deployment', () => {
+        it('Should set the right owner', async () => {
+            expect(await token.admin()).to.equal(admin.address);
+        });
+
+        it('Should transfer tokens between account', async () => {
+            await token.transfer(addr1.address, 50);
+            const addr1Balance = await token.balanceOf(addr1.address);
+            expect(addr1Balance).to.equal(50);
+
+            await token.connect(addr1).transfer(addr2.address, 50);
+            const addr2Balance = await token.balanceOf(addr2.address);
+            expect(addr2Balance).to.equal(50);
+        });
+
+        it("Should fail if sender doesn't have enough tokens", async () => {
+            const initialAdminBalance = await token.balanceOf(admin.address);
+
+            await expect(token.connect(addr1).transfer(admin.address, 1)).to.be.revertedWith('Insuffiencet balance');
+
+            expect(await token.balanceOf(admin.address)).to.equal(initialAdminBalance);
+        });
+
+        it('Should update balance after transfers', async () => {
+            const initialAdminBalance = await token.balanceOf(admin.address);
+
+            await token.transfer(addr1.address, 1000);
+            await token.transfer(addr2.address, 9000);
+
+            const newAdminBalance = await token.balanceOf(admin.address);
+
+            // big number overflow - use bignumber lib or reduce total supply for testing purpose
+            // expect(newAdminBalance).to.equal(initialAdminBalance - (1000 + 9000));
+            const addr1Balance = await token.balanceOf(addr1.address);
+            expect(addr1Balance).to.equal(1000);
+            const addr2Balance = await token.balanceOf(addr2.address);
+            expect(addr2Balance).to.equal(9000);
+        });
+    });
+});
